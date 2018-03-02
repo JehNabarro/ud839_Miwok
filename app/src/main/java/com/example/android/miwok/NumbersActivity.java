0833883fce140175f09da174cc08b20059887cf0
jessica nabarro <jeh.nabarro@gmail.com>
2018-03-02 17:54:27 -0300

    7 aula - Working with An ArrayList

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -24,5 +27,22 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //Create an array of words
+        String[] words = new String[10];
+
+        words[0] = "One";
+        words[1] = "Two";
+        words[2] = "Three";
+        words[3] = "Four";
+        words[4] = "Five";
+        words[5] = "Six";
+        words[6] = "Seven";
+        words[7] = "Eight";
+        words[8] = "Nine";
+        words[9] = "Tem";
+
+        Log.v("NumbersActivity", "Word at index 0: " + words[0]);
+
     }
 }
